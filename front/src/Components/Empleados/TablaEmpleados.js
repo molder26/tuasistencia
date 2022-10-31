68dd4d89480376648dacadce8207e60e80b3a34d
Pablo Szejpiacki <molder26@hotmail.com>
2022-10-31 19:25:38 -0300

    add button to data table

--- front/src/Components/Empleados/TablaEmpleados.js
+++ front/src/Components/Empleados/TablaEmpleados.js
@@ -1,7 +1,5 @@
 import  MUIDataTable  from "mui-datatables";
 
-
-const columns = ["Nombre", "Apellido", "Direccion", "Telefono"];
 const data = [
     ["jimmie", "klein", "oak lawn ave 526", "1-104-001-4567"],
     ["kate", "hale", "avondale ave 345", "1-678-456-1934" ],
@@ -10,6 +8,48 @@ const data = [
     
 ];
 const options = { filterType: 'checkbox'};
+
+const columns = [
+    {
+     name: "Nombre",
+     label: "Nombre",
+     options: {
+      filter: true,
+      sort: true,
+     }
+    },
+    {
+     name: "Apellido",
+     label: "Apellido",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+    {
+     name: "Direccion",
+     label: "Direccion",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+    {
+     name: "Telefono",
+     label: "Telefono",
+     options: {
+      filter: true,
+      sort: false,
+      customBodyRenderLite: (dataIndex, rowIndex) => {
+        return (
+          <button onClick={() => window.alert(`Clicked "Edit" for row ${rowIndex} with dataIndex of ${dataIndex}`)}>
+            Edit
+          </button>
+        );
+      }
+     }
+    },
+   ];
 
 export const TablaEmpleados = () => {
     return (
