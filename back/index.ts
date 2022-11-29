5fb971c0813f13822ad64ea1ab992388491588b9
Pablo Szejpiacki <molder26@hotmail.com>
2022-11-28 21:36:22 -0300

    socket working

--- back/index.ts
+++ back/index.ts
@@ -1,25 +1,35 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 const server = require("./src/app");
 const { conn } = require("./src/db");
 require("dotenv").config();
 // const { Server } = require("socket.io");
 // const io = new Server(server);
 const { PORT } = process.env;
-const socketIO = require('socket.io');
-const http = require('http')
+// const socketIO = require('socket.io');
 
-const httpServer = http.createServer(server);
-let io = socketIO(httpServer)
+// const http = require('http')
 
-io.on('connection', (socket:any) => {
-    console.log('a user connected')
-})
+// const httpServer = http.createServer(server);
+// let io = socketIO(httpServer)
 
-io.listen(10500);
+const httpServer = createServer(server);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://localhost:3000",
+	},
+});
 
+io.on("connection", (socket: any) => {
+	console.log("a user connected");
+});
 
-conn.sync({ force: false}).then(() => {
-	console.log('Modelos Sincronizados');
-    server.listen(PORT || 3001, () => {
-        console.log(`%s listening at ${PORT || 3001}`); // eslint-disable-line no-console
-    });
+// io.listen(10500);
+
+conn.sync({ force: false }).then(() => {
+	console.log("Modelos Sincronizados");
+	httpServer.listen(PORT || 3001, () => {
+		console.log(`%s listening at ${PORT || 3001}`); // eslint-disable-line no-console
+	});
 });
--- front/src/Components/Marcacion/BtnPresentismo.tsx
+++ front/src/Components/Marcacion/BtnPresentismo.tsx
@@ -3,55 +3,86 @@ import { Dialog } from "@mui/material";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
 import DialogContent from "@mui/material/DialogContent";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
+import { io } from "socket.io-client";
 
-
+const socket = io("localhost:3001");
 
 function BtnPresentismo() {
-  
-  let [open, setOpen] = React.useState(false);
+	let [open, setOpen] = React.useState(false);
 
-  let [state, setState] = React.useState(true);
+	let [state, setState] = React.useState(true);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-    setState(!state);
-    
-  };
+	const handleClickOpen = () => {
+		setOpen(true);
+		setState(!state);
+		// connectSocket();
+	};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  return (
-    <>
-      <Box>
-        <Button
-          style={{
-            margin: "0 0 0 -8%",
-            width: "25%",
-            padding: "0 30px 0 30px",
-            display: "flex",
-            alignContent: "center",
-            backgroundColor: state ? "" : "red",
-          }}
-          variant="contained"
-          onClick={handleClickOpen}
-        >
-          {state ? "Ingreso" : "Egreso"}
-        </Button>
-      </Box>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>CARGA DE PRESENTISMO</DialogTitle>
-        <br/>
-        <DialogContent>
-        <QRCode  value="Aca el valor que quieras dar" />
-        </DialogContent>
-      </Dialog>
+	// const connectSocket = () => {
+	// 	// client-side
+	// 	socket.on("connect", () => {
+	// 		console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+	// 	});
 
-    </>
-  );
+	// 	socket.on("disconnect", () => {
+	// 		console.log(socket.id); // undefined
+	// 	});
+	// };
+
+	const [isConnected, setIsConnected] = useState(socket.connected);
+
+	useEffect(() => {
+		socket.on("connect", () => {
+			setIsConnected(true);
+		});
+
+		socket.on("disconnect", () => {
+			setIsConnected(false);
+		});
+
+		return () => {
+			socket.off("connect");
+			socket.off("disconnect");
+			socket.off("pong");
+		};
+	}, []);
+
+	return (
+		<>
+			<Box>
+				<Button
+					style={{
+						margin: "0 0 0 -8%",
+						width: "25%",
+						padding: "0 30px 0 30px",
+						display: "flex",
+						alignContent: "center",
+						backgroundColor: state ? "" : "red",
+					}}
+					variant="contained"
+					onClick={handleClickOpen}
+				>
+					{state ? "Ingreso" : "Egreso"}
+				</Button>
+			</Box>
+			<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>CARGA DE PRESENTISMO</DialogTitle>
+				<br />
+				<DialogContent>
+					<QRCode value="Aca el valor que quieras dar" />
+				</DialogContent>
+			</Dialog>
+			<div>
+				<p>Connected: {"" + isConnected}</p>
+			</div>
+		</>
+	);
 }
 
 export default BtnPresentismo;
