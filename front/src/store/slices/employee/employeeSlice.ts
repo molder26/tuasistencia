5311a19ccce05f21bef87c9ad1e959b6900f2f4c
Pablo Szejpiacki <molder26@hotmail.com>
2023-04-13 00:22:12 -0300

    url backend hardcoded

--- front/src/store/slices/employee/employeeSlice.ts
+++ front/src/store/slices/employee/employeeSlice.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // import type { PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api";
+const API_URL = "http://localhost:80/api";
 
 export interface EmployeeState {
 	values: any[];
