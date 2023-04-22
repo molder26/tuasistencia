aa58dce8a753803ac45f0f0e88a07ed3d57d2495
Pablo Szejpiacki <molder26@hotmail.com>
2023-04-22 19:35:32 -0300

    add ip checking qr

--- front/src/Components/Qr/QrScanner.tsx
+++ front/src/Components/Qr/QrScanner.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export default function QrScanner() {
-    let {employeeId, stateId}= useParams();
+	let { employeeId, stateId } = useParams();
+	const [searchParams] = useSearchParams();
+	const [ipCliente, setIp] = useState(undefined);
 
-  return (
+	const ipServer = searchParams.get("ip");
+	// const ipCliente = await axios.get("https://api.ipify.org/?format=json");
 
-    <div>{employeeId}</div>
-  )
+	useEffect(() => {
+		async function getIp() {
+			const ipify = await axios.get("https://api.ipify.org/?format=json");
+			setIp(ipify.data.ip);
+		}
+		getIp();
+	}, []);
+
+	if (ipCliente === undefined) return <div>Cargando...</div>;
+	return (
+		<div>
+			{employeeId} - {ipCliente} - {ipServer} -{" "}
+			{ipCliente === ipServer ? "Son iguales" : "Son diferentes"}
+		</div>
+	);
 }
